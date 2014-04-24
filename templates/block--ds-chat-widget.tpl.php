5474ec4512477c22aa2af5ef90d095d71389944a
Margots Kapacs <makapacs@hotmail.com>
2014-04-24 17:56:05 -0500

    init commit

---
+++ templates/block--ds-chat-widget.tpl.php
@@ -0,0 +1,5 @@
+<div class="text-image" style="text-align:left">
+    <img src="/<?php print drupal_get_path('module', 'designssquare_com_ds')?>/assets/img/premium-themes-chat.png" alt="" width="36" height="43" />
+</div>
+<h3>CHAT WITH US</h3>
+<p>CURRENTLY OFFLINE</p>
---
+++ templates/block--ds-satisfaction-guaranteed-widget.tpl.php
@@ -0,0 +1,11 @@
+<div class="widget_banner">
+    <div  class="satisfaction_guaranteed group">
+        <div class="satisfaction-img">
+            <img src="/<?php print drupal_get_path('module', 'designssquare_com_ds')?>/assets/img/100_percent_banner_other_9.png" alt="satisfaction guaranteed"/>
+        </div>
+        <div class='wrap-message'>
+            <h3>Satisfaction Guarantee</h3>
+            <p>or money back</p>
+        </div>
+    </div>
+</div>
